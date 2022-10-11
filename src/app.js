0bb309aeb5dcb9f58b4047b35c201ca8adc8344a
Haovvh <Haovvh@gmail.com>
2022-10-11 13:04:59 +0500

    Done B1

--- src/app.js
+++ src/app.js
@@ -7,21 +7,21 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use( express.json() );
-app.use( express.urlencoded({ extended: false }) );
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 // Routes
 app.use('/api', require("./Routes/user.routes"));
 app.use('/api', require("./Routes/auth.routes"));
-app.use('/api', require("./Routes/product.routes"));
-app.use('/api', require("./Routes/category.routes"));
+app.use('/api', require("./Routes/Customer.routes"));
+app.use('/api', require("./Routes/Driver.routes"));
 
 // This folder will be Public
-app.use( express.static( path.join( __dirname, 'Uploads/Profile') ));
-app.use( express.static( path.join( __dirname, 'Uploads/Home' )));
-app.use( express.static( path.join( __dirname, 'Uploads/Products' )));
-app.use( express.static( path.join( __dirname, 'Uploads/Categories' )));
+app.use(express.static(path.join(__dirname, 'Uploads/Profile')));
+app.use(express.static(path.join(__dirname, 'Uploads/Home')));
+app.use(express.static(path.join(__dirname, 'Uploads/Products')));
+app.use(express.static(path.join(__dirname, 'Uploads/Categories')));
 
 
 module.exports = app;
